48fde9a1fcde6408296fde9c4c5765afe7a98ff9
hyupkim <makeyeezy@gmail.com>
2021-04-11 18:18:33 -0400

    add .gitignore

--- urlshortner/settings.py
+++ urlshortner/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import django_heroku
 import dj_database_url
-from ./secret_key import *
+from .secret_key import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = secret_key.SECRET_KEY
+SECRET_KEY = MY_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
